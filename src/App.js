8af57ba83aa354555f8b7a918ba0f032c24a3f31
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-15 17:02:48 -0400

    Add App render, constructor, and mount, and fetches

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let url = " https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			cocktails: [],
+		};
+	}
+	render() {
+		return <div className="App"></div>;
+	} //render
+
+	getCocktails = () => {
+		// * See https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/
+		Promise.all([fetch(url), fetch(url), fetch(url), fetch(url), fetch(url)])
+			.then((responses) => {
+				return Promise.all(
+					responses.map((res) => {
+						return res.json();
+					})
+				);
+			})
+			.then((data) => {
+				console.log(data);
+				// let arr = this.state.jokes;
+				// arr.push(data.joke);
+
+				// this.setState({
+				//   joke: data.joke,
+				//   jokes: arr,
+				// });
+			});
+	}; //getCocktail
+
+	componentDidMount() {
+		this.getCocktails();
+	} //componentDidMount
+} //App
 
 export default App;
