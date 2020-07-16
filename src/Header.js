67bac63a599ff31df4a22163a173e79006a162d8
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-15 23:11:24 -0400

    Add Header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+	return (
+		<header>
+			<h1>Five Fab Cocktails</h1>
+			<h3>Imbibe | Enjoy | Repeat</h3>
+		</header>
+	);
+} //Header
+
+export default Header;
