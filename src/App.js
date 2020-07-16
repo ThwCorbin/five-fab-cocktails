4d391f4b71152680a76904d23408c257341702f3
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-15 21:17:08 -0400

    Add Tabs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Tabs from "./Tabs.js";
 
 let url = " https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
@@ -9,12 +10,17 @@ class App extends Component {
 		this.state = {
 			cocktails: [],
 		};
-	}
+	} //constructor
 	render() {
-		return <div className="App"></div>;
+		return (
+			<div className="App">
+				<Tabs cNames={this.state.cocktails[0].name} />
+			</div>
+		);
 	} //render
 
 	getCocktails = () => {
+		// * Fetch five random cocktails
 		// * See https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/
 		Promise.all([fetch(url), fetch(url), fetch(url), fetch(url), fetch(url)])
 			.then((responses) => {
@@ -25,16 +31,45 @@ class App extends Component {
 				);
 			})
 			.then((data) => {
-				console.log(data);
-				// let arr = this.state.jokes;
-				// arr.push(data.joke);
-
-				// this.setState({
-				//   joke: data.joke,
-				//   jokes: arr,
-				// });
+				let objArr = [];
+				data.forEach((obj) => {
+					let cocktail = {
+						name: obj.drinks[0].strDrink,
+						instructions: obj.drinks[0].strInstructions,
+						image: obj.drinks[0].strDrinkThumb,
+						spirt: {
+							spirit1: obj.drinks[0].strIngredient1,
+							spirit2: obj.drinks[0].strIngredient2,
+							spirit3: obj.drinks[0].strIngredient3,
+							spirit4: obj.drinks[0].strIngredient4,
+							spirit5: obj.drinks[0].strIngredient5,
+							spirit6: obj.drinks[0].strIngredient6,
+							spirit7: obj.drinks[0].strIngredient7,
+							spirit8: obj.drinks[0].strIngredient8,
+							spirit9: obj.drinks[0].strIngredient9,
+							spirit10: obj.drinks[0].strIngredient10,
+						},
+						measure: {
+							measure1: obj.drinks[0].strMeasure1,
+							measure2: obj.drinks[0].strMeasure2,
+							measure3: obj.drinks[0].strMeasure3,
+							measure4: obj.drinks[0].strMeasure4,
+							measure5: obj.drinks[0].strMeasure5,
+							measure6: obj.drinks[0].strMeasure6,
+							measure7: obj.drinks[0].strMeasure7,
+							measure8: obj.drinks[0].strMeasure8,
+							measure9: obj.drinks[0].strMeasure9,
+							measure10: obj.drinks[0].strMeasure10,
+						},
+					};
+					objArr.push(cocktail);
+				});
+				console.log(objArr);
+				this.setState({
+					cocktails: objArr,
+				}); //setState
 			});
-	}; //getCocktail
+	}; //getCocktails
 
 	componentDidMount() {
 		this.getCocktails();
---
+++ src/Tabs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Tabs.css";
+
+function Tabs(prop) {
+	return (
+		<ul className="tabs">
+			<li className="c-name c-name-1"></li>
+			<li className="c-name c-name-2"></li>
+			<li className="c-name c-name-3"></li>
+			<li className="c-name c-name-4"></li>
+			<li className="c-name c-name-5"></li>
+		</ul>
+	); //return
+} //Tab
+
+export default Tabs;
