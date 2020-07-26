a33bfe09f20d9de02d17c2f3fd75a1adb37cd539
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-26 13:29:36 -0400

    Updates

---
+++ precache-manifest.5c3a73647fb02b3d68504e96895dd433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b2ddbc84a8f9007c93d27a779121ef",
+    "url": "/five-fab-cocktails/index.html"
+  },
+  {
+    "revision": "7a2792e958ef3ba8895e",
+    "url": "/five-fab-cocktails/static/css/main.dddbc30f.chunk.css"
+  },
+  {
+    "revision": "7a66481856261766a4c7",
+    "url": "/five-fab-cocktails/static/js/2.ac42d179.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/five-fab-cocktails/static/js/2.ac42d179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2792e958ef3ba8895e",
+    "url": "/five-fab-cocktails/static/js/main.d1c604ae.chunk.js"
+  },
+  {
+    "revision": "f59a72a53a063e17c41e",
+    "url": "/five-fab-cocktails/static/js/runtime-main.e32f1af7.js"
+  }
+]);
